5eb0c2544f56258b1dc0876ca2112ac4d87fd230
TechFounder <techfounder@outlook.com>
2013-06-13 21:15:17 -0400

    Add finished hw2 files

---
+++ hw2/hw2.js
@@ -0,0 +1,21 @@
+document.write('<table>');
+	for (row = 1; row <= 11; row++) {
+		if (row == 1) {
+			document.write('<th>', 'X', '</th>');
+				col = 1;
+				while (col <= 10) {
+						document.write('<th>', col, '</th>');
+						col++;
+				}
+		}
+		else {
+			document.write('<th>', row - 1, '</th>');
+				col = 1;
+				while (col <= 10) {
+						document.write('<td>', (row - 1) * col, '</td>');
+						col++;
+				}
+		}
+		document.write('<tr>');
+	}
+document.write('</table>');
---
+++ hw2/hw2ran.js
@@ -0,0 +1,22 @@
+document.write('<table>');
+var max = Math.floor((Math.random()*10)+1);
+	for (row = 1; row <= max + 1; row++) {
+		if (row == 1) {
+			document.write('<th>', 'X', '</th>');
+				col = 1;
+				while (col <= max) {
+						document.write('<th>', col, '</th>');
+						col++;
+				}
+		}
+		else {
+			document.write('<th>', row - 1, '</th>');
+				col = 1;
+				while (col <= max) {
+						document.write('<td>', (row - 1) * col, '</td>');
+						col++;
+				}
+		}
+		document.write('<tr>');
+	}
+document.write('</table>');
