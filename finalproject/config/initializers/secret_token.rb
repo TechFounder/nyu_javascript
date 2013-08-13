8556150db1bfbd2828e64aef33f43189eba05a15
TechFounder <techfounder@outlook.com>
2013-08-13 14:15:02 -0400

    Add finalproject

---
+++ finalproject/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Finalproject::Application.config.secret_token = '1089f4fb7ae9bbd9df7adf6575d43c5863a9e0aac997292aef152b7072fef8734cac9f977a367b74ba339294f9adff2e4d08c0f113377de77a4d663f0fae3e28'
